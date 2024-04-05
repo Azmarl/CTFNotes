e5a9509fac49ab0ade1ae0efef756fecb6aee0b3
Azmarl <yzy3568653@163.com>
2024-04-05 21:38:08 +0800

    update

---
+++ Azmarl/web/黑名单字符fuzz脚本/fuzz.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+# Author: m0c1nu7
+import requests
+
+def ascii_str():
+	str_list=[]
+	for i in range(33,127):
+		str_list.append(chr(i))
+	#print('可显示字符：%s'%str_list)
+	return str_list
+
+def upload_post(url):
+	str_list = ascii_str()
+	for str in str_list:
+		header = {
+		'Host':'3834350a-887f-4ac1-baa4-954ab830c879.node3.buuoj.cn',
+		'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0',
+		'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'Accept-Language':'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+		'Accept-Encoding':'gzip, deflate',
+		'Content-Type':'multipart/form-data; boundary=---------------------------339469688437537919752303518127'
+		}
+		post = '''-----------------------------339469688437537919752303518127
+Content-Disposition: form-data; name="file"; filename="test.txt"
+Content-Type: text/plain
+
+12345'''+str+'''
+-----------------------------339469688437537919752303518127
+Content-Disposition: form-data; name="submit"
+
+提交			
+-----------------------------339469688437537919752303518127--'''
+
+		res = requests.post(url,data=post.encode('UTF-8'),headers=header)
+		if 'Stored' in res.text:
+			print("该字符可以通过:  {0}".format(str))
+		else:
+			print("过滤字符:  {0}".format(str))
+			
+
+
+if __name__ == '__main__':
+	url = 'http://3834350a-887f-4ac1-baa4-954ab830c879.node3.buuoj.cn/index.php?act=upload'
+	upload_post(url)
